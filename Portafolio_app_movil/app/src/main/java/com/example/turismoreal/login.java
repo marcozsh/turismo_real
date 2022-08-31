ea8c4d75bc4d603fb2233a307336164073dafba7
marcozsh <marc.penar@outlook.cl>
2022-08-30 21:14:11 -0400

    first-commit

---
+++ Portafolio_app_movil/app/src/main/java/com/example/turismoreal/login.java
@@ -0,0 +1,16 @@
+package com.example.turismoreal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.Theme_TurismoReal);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
