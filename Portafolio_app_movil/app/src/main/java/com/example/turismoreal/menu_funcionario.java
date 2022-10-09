5bf7fb7334946ea9e48fc3a1aab104264bc96977
kev1inm <ke.navarretem@duocuc.cl>
2022-10-09 12:09:01 -0300

    "Prueba interfaz 1"

---
+++ Portafolio_app_movil/app/src/main/java/com/example/turismoreal/menu_funcionario.java
@@ -0,0 +1,14 @@
+package com.example.turismoreal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_funcionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_funcionario);
+    }
+}
